6c830d5e3571016ab4f752157a28d89427bf5f49
korsoserik <korsos.erik@students.jedlik.com>
2024-01-30 14:12:53 +0100

    orai munka

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Chess
 {
@@ -22,35 +23,65 @@ namespace Chess
     {
         public MainWindow()
         {
-            for (int i = 0; i < 12+1; i++)
-            {
-                
-            }
 
             InitializeComponent();
+            Loadcbx();
+        }
+        void Loadcbx()
+        {
+            for (int i = 3; i <= 7; i++)
+            {
+                CollumnsCountCbx.Items.Add(i);
+                RowsCountCbx.Items.Add(i);
+            }
+            CollumnsCountCbx.SelectedIndex = 2;
+            RowsCountCbx.SelectedIndex = 2;
         }
 
-        private bool[,] minefield = new bool[5, 5];
+
+        void ClearField()
+        {
+            Field.Children.Clear();
+            Field.RowDefinitions.Clear();
+            Field.ColumnDefinitions.Clear(); 
+        }
 
         void looadField()
         {
-
+            for (int i = 0; i < int.Parse(RowsCountCbx.SelectedItem.ToString()); i++)
+            {
+                RowDefinition r1 = new RowDefinition();
+                
+                Field.RowDefinitions.Add(r1);
+                for (int j = 0; j < int.Parse(CollumnsCountCbx.SelectedItem.ToString()); j++)
+                {
+                    ColumnDefinition c1 = new ColumnDefinition();
+                    Field.ColumnDefinitions.Add(c1);
+                }
+            }
         }
 
         private void InitializeMinefield()
         {
             Random random = new Random();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < int.Parse(RowsCountCbx.SelectedItem.ToString()); i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < int.Parse(CollumnsCountCbx.SelectedItem.ToString()); j++)
                 {
-                    minefield[i, j] = random.Next(5) == 0; // 1 in 5 chance of having a mine
+                   
                 }
             }
         }
 
+        void Cell_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void startBtn_Click(object sender, RoutedEventArgs e)
         {
+            ClearField();
+            looadField();
 
         }
     }
