1977881e7d5870e7427b44e63e2b763a89b48503
Marcell Krupanszky <marci.krupanszky@gmail.com>
2024-02-12 11:05:24 +0100

    asd

--- Chess/login.xaml.cs
+++ Chess/login.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,51 @@ namespace Chess
     /// </summary>
     public partial class login : Window
     {
+        ObservableCollection<user> users = new ObservableCollection<user>();
         public login()
         {
             InitializeComponent();
+            fileRead();
+        }
+
+        void fileRead()
+        {
+            StreamReader sr = new StreamReader("Logininformation.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                users.Add(new user(sr.ReadLine()));
+            }
+            sr.Close();
+
+        }
+
+        private void UserLoginBTN_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 0;
+            bool létezike = false;
+            foreach (var item in users)
+            {
+
+                if (item.name == UsernameTXB.Text && item.password == PasswordTXB.Text)
+                {
+                    létezike = true;
+                    MainWindow main = new MainWindow(users[index]);
+                    this.Close();
+                    main.ShowDialog();
+
+                }
+                index++;
+            }
+            if (!létezike)
+            {
+                MessageBox.Show("Nem létezik ilyen felhasználo");
+            }
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Chess/user.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public DateTime freereward { get; set; }
+
+        public user(string sor)
+        {
+            string[] data = sor.Split(';');
+            id = int.Parse(data[0]);
+            name = data[1];
+            password = data[2];
+            freereward  = DateTime.Parse(data[3]);
+        }
+    }
+}
